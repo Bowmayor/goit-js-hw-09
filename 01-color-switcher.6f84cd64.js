feeef286b8f2959bfdb67b41f62b77c1f47d80ac
Bowmayor <31702247+Bowmayor@users.noreply.github.com>
2023-05-03 23:54:48 +0000

    Deploying to gh-pages from @ Bowmayor/goit-js-hw-09@8414606c8ed7fa7355fbe096356c90fdbd7ecc8a 🚀

--- 01-color-switcher.6f84cd64.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let l=null;t.addEventListener("click",(()=>{null===l&&(l=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.disabled=!0)})),e.addEventListener("click",(()=>{clearInterval(l),l=null,t.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.6f84cd64.js.map
